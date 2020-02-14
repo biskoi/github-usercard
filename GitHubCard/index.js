394d61e02e42cb516e8ef72ea609c5e3d1723e87
biskoi <biskoi@daydream.cafe>
2020-02-13 18:17:54 -0700

    tidied

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,30 +2,15 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/biskoi
 */
-
-// let cardsParent = document.querySelector('.cards');
-
-// axios.get('https://api.github.com/users/biskoi').then(reply => {
-//   console.log(reply.data.login);
-
-// });
-
-// axios.get('https://api.github.com/users/biskoi/followers').then(reply => {
-//   console.log(reply);
-// });
-
-
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
 
    Skip to Step 3.
 */
-
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
-
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
@@ -35,7 +20,6 @@
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
-
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -83,37 +67,36 @@ function cardMaker(attr){
   cardUsername.textContent = attr.login;
   cardProfileLink.textContent = attr.html_url;
   cardProfileLink.href = attr.html_url;
-  cardLocation.textContent = 'Location: ' + attr.location;
+  cardBio.textContent = `Bio: ${attr.bio} `;
+  cardLocation.textContent = `Location: ${attr.location}`;
 
   let followersLink = attr.url + '/followers';
-  axios.get(followersLink).then(reply => {
-    let length = reply.data.length;
-    cardFollowers.textContent = `Followers: ${length}`;
-  });
-  
   let followingLink = attr.url + '/following';
-  axios.get(followingLink).then(reply => {
-    let length = reply.data.length;
-    cardFollowing.textContent = `Following: ${length}`;
+
+  axios.get(followersLink).then(reply => {
+    cardFollowers.textContent = `Followers: ${reply.data.length}`;
   });
 
-  cardBio.textContent = `Bio: ${attr.bio} `;
+  axios.get(followingLink).then(reply => {
+    cardFollowing.textContent = `Following: ${reply.data.length}`;
+  });
 
   return card;
 
 }
 
-// const followersArray = ['sonicemma', 'MMGroesbeck', 'alan5123', 'fjhansen', 'MelodyRackham'];
+// let followersArray = ['sonicemma', 'MMGroesbeck', 'alan5123', 'fjhansen', 'MelodyRackham'];
 let followersArray = [];
-
 let cardsParent = document.querySelector('.cards');
 let mainUser = 'https://api.github.com/users/biskoi';
 let apiLink = 'https://api.github.com/users/';
+
 
 // Subject card
 axios.get(mainUser).then(reply => {
   cardsParent.prepend(cardMaker(reply.data));
 });
+
 
 // Get followers from Subject
 async function getFollowers(){
@@ -140,14 +123,4 @@ async function getFollowers(){
 }
 
 getFollowers();
-console.log(followersArray);
-
-// Follower cards
-// followersArray.forEach(user => {
-//   let url = apiLink + user;
-//   axios.get(url).then(reply => {
-//     cardsParent.append(cardMaker(reply.data));
-//   });
-
-// });
 
